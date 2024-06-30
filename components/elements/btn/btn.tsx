41a76319e8654acf2e125d4f2a7789fa89b5da7b
Sara Vahidipour <m.vahidipour>
2024-06-30 14:35:03 +0330

    add css style and type btn

--- components/elements/btn/btn.tsx
+++ components/elements/btn/btn.tsx
@@ -1,9 +1,21 @@
+import { Button } from '@mui/material';
 import React from 'react'
+import '../elements/element.css'
+import { BtnType } from './btn.type';
+import '../../../globals.css';
 
-export default function btn() {
+export default function btn({ title, colorTitle, bgColor, disableColor, error, click, fontSize }: BtnType) {
   return (
     <div>
-      
+      <Button
+        className={error ? (disableColor && fontSize && colorTitle) :
+          bgColor ? (bgColor && fontSize && colorTitle) :
+            "defaultBgColor whiteFontColor fontSize"}
+        disabled={error ? true : false}
+        onClick={() => click}
+      >
+        {title}
+      </Button>
     </div>
   )
 }
